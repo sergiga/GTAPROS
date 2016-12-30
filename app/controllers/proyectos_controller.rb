b1a372f76c864aba12a38a63ef8638fef469f37e
Pablo <simplepablete@gmail.com>
2016-12-30 05:04:53 +0100

    fixed some issues with date validations

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -1,3 +1,5 @@
+require 'time'
+require 'date'
 class ProyectosController < ApplicationController
   
   def index
@@ -16,9 +18,18 @@ class ProyectosController < ApplicationController
     projParams = params[:session]
     @errors = ""
 
-    if ((DateTime.parse(projParams[:startDate]) rescue ArgumentError) == ArgumentError)
+    start_date = projParams[:startDate] 
+
+    begin
+    DateTime.parse start_date
+    rescue 
+      @errors = 'Fecha incorrecta' 
+      return 
+    end
+
+    if (DateTime.parse start_date) < DateTime.now.to_date
       @errors = 'Fecha incorrecta'
-      #TODO error si el formato de la fecha es incorrecto
+      return
     end
 
     proyecto = Proyecto.find_by(
@@ -45,29 +56,27 @@ class ProyectosController < ApplicationController
     @errors = ""
     manager_username = params[:session][:username]
     project_id = params[:id]
-    puts "project_id ==>> " + project_id
 
     manager = Empleado.find_by(usuario: manager_username)
 
     if !manager
-      #TODO error no existe el usuario
       @errors = "el unuario no existe"
       return
     elsif manager.categoria != 1
-      #TODO el empleado no tiene categoria para ser jefe de proyecto
       @errors = "el empleado no tiene categoría suficiente para ser jefe de proyecto"
       return
     end 
 
-    projects_assigned_to_manager = AsignacionProyecto.find_by(empleado_id: manager.id, rol: 1)
-
+    projects_assigned_to_manager = AsignacionProyecto.joins(:proyecto).where(empleado_id: manager.id, rol: 1).
+                                                  where("fecha_inicio < ?", DateTime.now.to_date).
+                                                  where("fecha_fin > ?", DateTime.now.to_date)
+                                              
     if !projects_assigned_to_manager
       puts "se crea"
       proyecto = Proyecto.find_by(id: project_id)
       AsignacionProyecto.create(empleado_id: manager.id, proyecto_id: project_id, rol: 1, participacion: 35)
       redirect_to proyectos_path and return
     else
-      #TODO el empleado ya tiene asignado en esas fechas un proyecto
       @errors = "ya es jefe de proyecto de otro proyecto"
     end
   end
