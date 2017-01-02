00d85d4c94a42eda615be54946ddbdace0ddeed2
Sergio <srgarcia91@gmail.com>
2017-01-02 18:34:08 +0100

    Fixed bug with resources order

--- config/routes.rb
+++ config/routes.rb
@@ -9,21 +9,25 @@ Rails.application.routes.draw do
   resources :empleados do
       resources :proyectos, only: [:index, :show]
   end
-  resources :proyectos, only: [:show, :edit, :update, :destroy, :create] do
+
+  resources :proyectos, only: [:new, :create]
+  get   'proyectos/setmanager', to: 'proyectos#setmanager'
+  post  'proyectos/setmanager', to: 'proyectos#setmanagerforproject'
+  resources :proyectos, only: [:show, :edit, :update, :destroy] do
     resources :actividads, only: [:new, :create]
     resources :asignacion_proyectos, only: [:new, :create]
   end
+  
   resources :actividads, only: [:show]
   get 'actividads/:id/setempleado', to: 'actividads#setempleadonew', as: 'new_actividad_setempleado'
   post 'actividads/:id/setempleado', to: 'actividads#setempleadocreate', as: 'create_actividad_setempleado'
 
   get 'proyectos' => 'proyectos#all'
-  get   'proyectos/new'
-  post  'proyectos/new', to: 'proyectos#create'
-  get   'proyectos/all', to: 'proyectos#all'
-  get   'proyectos/setmanager', to: 'proyectos#setmanager'
-  post  'proyectos/setmanager', to: 'proyectos#setmanagerforproject'
-  delete '/proyectos', to: 'proyectos#delete'
-  put 'proyectos/:id/init', to: 'proyectos#init', as: 'proyectos_init'
-  resources :proyectos
+#  get   'proyectos/new'
+#  post  'proyectos/new', to: 'proyectos#create'
+#  get   'proyectos/all', to: 'proyectos#all'
+#  get   'proyectos/setmanager', to: 'proyectos#setmanager'
+#  post  'proyectos/setmanager', to: 'proyectos#setmanagerforproject'
+#  delete '/proyectos', to: 'proyectos#delete'
+#  put 'proyectos/:id/init', to: 'proyectos#init', as: 'proyectos_init'
 end
